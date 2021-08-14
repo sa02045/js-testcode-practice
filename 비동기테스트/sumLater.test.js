333f89900df2068a4fc2f9ceba39d1d9b670b9b3
DevJo <sa02045@naver.com>
2021-08-14 23:38:08 +0900

    async await를 이용한 비동기 테스트

--- 비동기테스트/sumLater.test.js
+++ 비동기테스트/sumLater.test.js
@@ -9,3 +9,8 @@ test("1+3의 2초후 합한 계산값은 4", () => {
 test("1+3의 2초후 합한 계산값은 4", () => {
   return expect(sumLater(1, 3)).resolves.toBe(4);
 });
+
+test("1+3의 2초후 합한 계산값은 4", async () => {
+  const result = await sumLater(1, 3);
+  expect(result).toBe(4);
+});
